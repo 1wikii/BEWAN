16cbe343091e633b406ea7d530107d43391bf724
ahmaddwiky <zerro140766@gmail.com>
2023-04-28 20:32:56 +0700

    update

--- bewan.py
+++ bewan.py
@@ -1,75 +1,90 @@
 import pygame as pg
 import random as rd
 
-pg.init()
-screen = pg.display.set_mode((720,500))
-cl = pg.time.Clock()
-running = True
-
-pg.display.set_caption('TYPING GAME by wikii')
-screen.fill("purple")
-font = pg.font.SysFont('comicsansms',36)
-
 # COLOR
 purple = (189, 52, 235)
 white = (255, 255, 255)
 orange = (242, 103, 10)
+black = (0, 0, 0)
 
 # WORD LIST 
 word_list = ['nangka','nanas','semangka','durian']
 score = 0
 
 # Handling each letter color
-class WORD:
+class EACH_WORD:
 
 	def __init__(self):
-		self.color = white
+		self.__color = black
 		self.word = rd.choice(word_list)
+	
+	@property
+	def color(self):
+		return self.__color
 
 	def typed(self):
-		self.color = orange
+		self.__color = orange
 
-	def left_right(self):
+	def move_to_right(self):
 		pass
 
-	def right_left(self):
+	def move_to_left(self):
 		pass
+	
+	# abstraction concept hiding another method to make it simple
+	def word_moving(self, direction):
+		if direction == "right":
+			self.move_to_right()
+		
+		elif direction == "left":
+			self.move_to_left()
 
-	def draw(self):
-		letter_surf = font.render(self.ch, True, white)
-		screen.blit(letter_surf, (100,100))
 
 def correct(word):
+	word = list(word)
 	word.pop(0)
+	word = ''.join(word)
 	return word
 
 def word_empty(word):
 	return len(word) == 0
 
 def make_obj():
-	return WORD()
+	return EACH_WORD()
 
-xx = screen.get_rect().width/2
-yy = 400
+pg.init()
+screen = pg.display.set_mode((720,500))
+cl = pg.time.Clock()
+pg.display.set_caption('BEWAN')
+font = pg.font.SysFont('comicsansms',36)
+running = True
 
-word = make_obj()
+main_word = make_obj()
+the_word = main_word.word
+
 while running:
-	pos = 0
-	
+
+	screen.fill("purple")
+
+	show = font.render(the_word, True, main_word.color)
+	screen.blit(show, (200,200))
+
+	sc = font.render( f"Score : {score}", True, black)
+	screen.blit (sc, (10,10))
+
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
 			running = False
-		
+
 		if event.type == pg.KEYDOWN:
-			for idx in range(len(word)):
-				if chr(event.key) == word[0].letter:
-					correct(word)
-				if word_empty(word):
-					word = make_obj()
+
+			# for idx in range(len(the_word)):
+			if chr(event.key) == the_word[0]:
+				the_word = correct(the_word)
+				if word_empty(the_word):
+					main_word = make_obj()
+					the_word = main_word.word
 					score += 1
-	
-	sc = font.render( f"Score : {score}", True, black)
-	screen.blit (sc, (10,10))
 	
 	pg.display.update()
 	cl.tick(60)  # 60 fps
