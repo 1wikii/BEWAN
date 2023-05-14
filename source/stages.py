58bd19119155bfaea423c8da2dfd4275808fc0c2
Ahmad Dwiky Zerro Dixxon <92775985+1wikii@users.noreply.github.com>
2023-05-14 15:08:57 +0700

    DELETE

--- source/stages.py
+++
@@ -1,6 +0,0 @@
-word_gap = 80
-
-
-stage_1 = {"number_of_word":4, "speed_min":0, "speed_max": 5.5}
-stage_2 = {"number_of_word":8, "speed_min":0, "speed_max": 5.5}
-stage_3 = {"number_of_word":12, "speed_min":0, "speed_max": 5.5}
